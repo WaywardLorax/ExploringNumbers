3cca4ff45743c725bce35545af4ce615ac2aba9f
waywardlorax <larson.anthony.r@gmail.com>
2021-05-20 12:26:05 -0400

    Created check for argument being passed into function and set the argument to be an integer

--- factoring.R
+++ factoring.R
@@ -2,7 +2,12 @@
 #To run, click the -> Source button above.  Then type find_factors() into the console and hit return
 ## 
 find_factors <- function(x){ #defining function and giving it the name find_factors
-testnumber <- as.integer(readline(prompt="What number do you want to factor?")) #store user response to promt in variable testnumber
+  if(hasArg(x)){    #detect if an argument x has been passed into the function
+    testnumber <-as.integer(x)  #if yes, then store this number as test number, making sure it is an integer
+  }
+  else{   #if no argument, prompt the user for a number to factor
+    testnumber <- as.integer(readline(prompt="What number do you want to factor?")) #store user response to promt in variable testnumber
+  }
 factors <- c(1) #create vector to store factors, with 1 as first value since all numbers are divisible by 1
 count <- 2   #begin testing factors with 2
 
@@ -11,7 +16,7 @@ while (count < testnumber) {
   if( testnumber %% count == 0){  #Check to see if testnumber divided by count has no remainder
     factors[length(factors) + 1] <- count #save the value of count at the end of the vector containing the factors
     print(count) #when a factor is found, print it out
-  }
+}
   count = count +1 #each loop, increment count by +1
 }
 factors[length(factors) + 1] <- testnumber #add the original number to the end of the factor list
